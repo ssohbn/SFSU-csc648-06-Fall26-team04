4be6b9097ddec0478eb0ace016bd525b5b0d92d9
ssohbn <92878876+ssohbn@users.noreply.github.com>
2026-09-13 12:34:23 -0700

    crap line goodbye

--- app/main.go
+++ app/main.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Welcome to my website!")
 		http.Redirect(w, r, "/static/about.html", http.StatusSeeOther)
 	})
 
